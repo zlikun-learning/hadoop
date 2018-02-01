436424d862e81491a57acc130ae12da2c0ebf50a
zlikun <zlikun-dev@hotmail.com>
2018-02-01 21:55:11 +0800

    put的cas用法

--- 07-hbase/src/test/java/com/zlikun/learning/PutTest.java
+++ 07-hbase/src/test/java/com/zlikun/learning/PutTest.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
@@ -57,6 +58,55 @@ public class PutTest {
         assertEquals("zlikun", Bytes.toString(result.getValue(family, Bytes.toBytes("name"))));
         assertEquals(17, Bytes.toInt(result.getValue(family, Bytes.toBytes("age"))));
 
+    }
+
+    @Test
+    public void putList() throws IOException {
+
+        final byte[] family = Bytes.toBytes("info");
+
+        Put put = new Put(Bytes.toBytes("0001"), System.currentTimeMillis());
+        put.addColumn(family, Bytes.toBytes("name"), Bytes.toBytes("zlikun"));
+        put.addColumn(family, Bytes.toBytes("age"), Bytes.toBytes(17));
+
+        Put put2 = new Put(Bytes.toBytes("0002"), System.currentTimeMillis());
+        put2.addColumn(family, Bytes.toBytes("name"), Bytes.toBytes("Ashe"));
+        put2.addColumn(family, Bytes.toBytes("gender"), Bytes.toBytes("female"));
+
+        // 批量写入
+        table.put(Arrays.asList(put, put2));
+
+        // 可以在HBase服务上通过scan命令查看数据是否成功写入
+        /*
+        > scan 'user'
+        ROW                     COLUMN+CELL
+         0001                   column=info:age, timestamp=1517492549195, value=\x00\x00\x00\x11
+         0001                   column=info:name, timestamp=1517492549195, value=zlikun
+         0002                   column=info:gender, timestamp=1517492548930, value=female
+         0002                   column=info:name, timestamp=1517492548930, value=Ashe
+        2 row(s) in 0.0210 seconds
+         */
+    }
+
+    @Test
+    public void check_and_put() throws IOException {
+
+        final byte[] family = Bytes.toBytes("info");
+
+        // 使用列族和时间戳构造Put实例
+        Put put = new Put(Bytes.toBytes("0001"), System.currentTimeMillis());
+        // 添加两列(Cell)，即：插入两条记录
+        put.addColumn(family, Bytes.toBytes("name"), Bytes.toBytes("zlikun"));
+        put.addColumn(family, Bytes.toBytes("age"), Bytes.toBytes(17));
+
+        // check-in-put，即：检查写，保证检查与写入的原子性，检查的数据存在时，才写入
+        // 本例意为：当版本号为1时，执行写入，否则不写入
+        table.checkAndPut(Bytes.toBytes("0001"), family, Bytes.toBytes("version"), Bytes.toBytes("1"), put);
+
+        // 验证写入
+        Result result = table.get(new Get(Bytes.toBytes("0001")));
+        assertEquals("zlikun", Bytes.toString(result.getValue(family, Bytes.toBytes("name"))));
+        assertEquals(24, Bytes.toInt(result.getValue(family, Bytes.toBytes("age"))));
     }
 
 }
